a51b981e59966ec8cd14973bb1ba605ed163ca69
Anson Miu <ansonmiu0214@gmail.com>
2018-09-23 20:09:55 +0800

    Added JSON export functionality to Python script; reads input from images folder and outputs to deeplens_data directory

--- deeplens/main.py
+++ deeplens/main.py
@@ -5,7 +5,15 @@ from keras.preprocessing.image import img_to_array
 from keras.models import model_from_json
 from PIL import Image
 import numpy as np
-import io
+import simplejson as json
+from glob import glob
+from datetime import datetime
+import os
+
+RESNET_DIMENSIONS = (224, 224)
+FLIGHT_CODE = "SQ856"
+SOURCE_IMAGE_DIR = "../source_images"
+DEEPLENS_DATA_DIR = "../deeplens_data"
 
 def load_model():
   global model
@@ -18,21 +26,41 @@ def prepare_image(image, target):
     image = image.convert("RGB")
 
   image = image.resize(target)
+  image.show()
+  
   image = img_to_array(image)
   image = np.expand_dims(image, axis=0)
   image = imagenet_utils.preprocess_input(image)
 
   return image
+
+def parseImage(path):
+  image = Image.open(path)
+
+  image = prepare_image(image, target=RESNET_DIMENSIONS)
+
+  predictions = model.predict(image)
+  results = imagenet_utils.decode_predictions(predictions)
+
+  data = { classifier: float(confidence) for _, classifier, confidence in results[0] }
+
+  timestamp = str(datetime.now())
+  json_payload = { 
+    "timestamp": timestamp,
+    "flightCode": FLIGHT_CODE,
+    "payload": data
+  }
+
+  basename, _ = os.path.basename(path).split('.')
+  json_filename = "{}/{}.json".format(DEEPLENS_DATA_DIR, basename)
   
+  with open(json_filename, 'w') as json_file:
+    json.dump(json_payload, json_file, indent=4)
+  
+  print("Exported results for {}".format(basename))
+
 
 if __name__ == "__main__":
   load_model()
-  image = Image.open("food3.jpeg")
-
-  image = prepare_image(image, target=(224, 224))
-
-  preds = model.predict(image)
-  results = imagenet_utils.decode_predictions(preds)
-
-  for idx, classifier, confidence in results[0]:
-    print("{}: {}".format(classifier, confidence))
+  for filename in glob("{}/*.jpeg".format(SOURCE_IMAGE_DIR)):
+    parseImage(filename)
