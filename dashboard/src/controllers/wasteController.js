c80555f81ee9d855acf41fdbcc4a5f469c0d4afc
Anson Miu <ansonmiu0214@gmail.com>
2018-09-28 11:10:34 +0100

    Implemented waste controller logic for mock data

--- dashboard/src/controllers/wasteController.js
+++ dashboard/src/controllers/wasteController.js
@@ -1,7 +1,37 @@
+const fs = require('fs')
+const path = require('path')
+
+const DATA_DIR = './../mock_data'
+
 function getWastage(req, res, next) {
   const { date, flightCode}  = req.params
 
-  res.send(`Waste data for flight ${flightCode} on ${date}`)
+  // Check whether there is wastage data
+  const targetDir = path.join(DATA_DIR, date, flightCode)
+  console.log(targetDir)
+
+  if (!fs.existsSync(targetDir)) return next(new Error(`No wastage data recorded for flight ${flightCode} on ${date}`))
+
+  // Read JSONs and return result
+  const dirPromise = new Promise((resolve, reject) => {
+    fs.readdir(targetDir, (error, files) => {
+      if (error) return reject(error)
+
+      const objects = files.map(file => {
+        const rawJSON = fs.readFileSync(path.join(targetDir, file))
+        return JSON.parse(rawJSON)
+      })
+
+      resolve(objects)
+    })
+  })
+
+  dirPromise.then(objects => {
+    console.log(objects)
+    res.status(200).json(objects)
+  }).catch(error => next(error))
+
+  // res.send(`Waste data for flight ${flightCode} on ${date}`)
 }
 
 module.exports = {
