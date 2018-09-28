7c9f4931d492e3556ea0b1f6184230e3ae6e164f
Anson Miu <ansonmiu0214@gmail.com>
2018-09-28 10:46:50 +0100

    Implemented getMeal route

--- dashboard/src/controllers/mealController.js
+++ dashboard/src/controllers/mealController.js
@@ -1,7 +1,25 @@
+const rp = require('request-promise')
+
 function getMeal(req, res, next) {
   const { date, flightCode}  = req.params
 
-  res.send(`Meal data for flight ${flightCode} on ${date}`)
+  const postOptions = {
+    uri: 'https://apigw.singaporeair.com/appchallenge/api/meal/upliftplan',
+    method: 'POST' ,
+    headers: {
+      'Content-Type': 'application/json',
+      'apikey': 'aghk73f4x5haxeby7z24d2rc'
+    },
+    body: {
+      flightNo: flightCode,
+      flightDate: date
+    },
+    json: true
+  }
+
+  rp(postOptions)
+    .then(parsedBody => res.status(200).json(parsedBody.response))
+    .catch(_ => next(new Error(`Meals for flight ${flightCode} on ${date} not found.`)))
 }
 
 module.exports = {
